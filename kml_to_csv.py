64a829069d7103a5a637bf9aa9b3657fb3f94176
Brendan Lai <51245035+Brendanlai@users.noreply.github.com>
2020-07-23 18:04:47 -0400

    Update kml_to_csv.py
    making a change to capture the elevation value as well (comma_split_2)

--- kml_to_csv.py
+++ kml_to_csv.py
@@ -8,7 +8,7 @@ def process_coordinate_string(str):
     # Take the coordinate string from the KML file, and break it up into [Lat,Lon,Lat,Lon...] for a CSV row
     ret = []
     comma_split = str.split(',')
-    return [comma_split[1], comma_split[0]] #Return (lat, long)
+    return [comma_split[1], comma_split[0], comma_split[2]] #Return (lat, long, elevation)
 
 def main():    
     # Open the KML. Read the KML. Open a CSV file. Process a coordinate string to be a CSV row.
