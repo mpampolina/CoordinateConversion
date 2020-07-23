67b2c47da6ad85febd4fe786546b9133d66eb3ef
Brendan Lai <51245035+Brendanlai@users.noreply.github.com>
2020-07-23 17:59:59 -0400

    kml to csv parser returning just the lat,lon
    make sure that your kml file is in the same directory. Note: might have to change/adjust the kml filename so that they match.

---
+++ kml_to_csv.py
@@ -0,0 +1,25 @@
+# Import dependecies
+
+from bs4 import BeautifulSoup
+import csv
+
+
+def process_coordinate_string(str):    
+    # Take the coordinate string from the KML file, and break it up into [Lat,Lon,Lat,Lon...] for a CSV row
+    ret = []
+    comma_split = str.split(',')
+    return [comma_split[1], comma_split[0]] #Return (lat, long)
+
+def main():    
+    # Open the KML. Read the KML. Open a CSV file. Process a coordinate string to be a CSV row.
+    # Make sure "profile.kml" or whatever the file is called is in the same directory
+    with open('profile.kml', 'r') as f:
+        s = BeautifulSoup(f, 'xml')
+        with open('profile.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+
+            for coords in s.find_all('coordinates'):
+                writer.writerow(process_coordinate_string(coords.string))
+
+# Run the main method
+main() 
