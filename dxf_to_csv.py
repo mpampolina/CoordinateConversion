426f686ceb4f5e513bfceb818c957a2df8358d07
Miguel Pampolina <miguel.r.pampolina@gmail.com>
2020-07-24 19:09:32 -0700

    added: dxf_to_csv.py

---
+++ dxf_to_csv.py
@@ -0,0 +1,48 @@
+import csv
+import re
+import os
+
+utmRegex = re.compile(r'''(AcDbPoint\n\s10\n(\d+\.\d+)\n\s20\n(\d+\.\d+)\n\s30\n(\d+\.\d+))
+''', re.VERBOSE)
+
+
+def dxfParser(Path):
+    with open(Path, 'r') as myfile:
+        data = myfile.read()    # reads in dxf.txt file and returns the data in string form
+
+        data = utmRegex.findall(data)   # returns a list of tuples. Each tuple corresponds to a coordinate set
+
+        coordinateList = []
+
+        for entry in data:
+            coordinates = {'Easting': entry[1], 'Northing': entry[2], 'Elevation': entry[3]}
+            coordinateList.append(coordinates)
+
+        with open('profile.csv', 'w', newline='') as csvFile:
+            fieldnames = ['Easting', 'Northing', 'Elevation']
+            writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for coordindateDict in coordinateList:
+                writer.writerow(coordindateDict)
+
+        scriptDirectory = os.getcwd()
+        print(f'Conversion Complete. Please check [{scriptDirectory}] for the converted file.')
+
+        print("Press Enter to continue ...")
+        input()
+
+
+def pathInput():
+    print('DXF to CSV Conversion Tool'.center(40, '='))
+    print('\nTo start your conversion, please enter one of the following options:')
+    print('\nOption-1: Please enter the path for your .dxf file.')
+    print('''\nOption-2: Ensure that the selected DXF file is located in the same directory as this script and
+please enter the filename for your .dxf file.\n''')
+    Path = input('Input: ')
+    return Path
+
+
+if __name__ == "__main__":
+    path = pathInput()
+    dxfParser(path)
--- kml_to_csv.py
+++ kml_to_csv.py
@@ -1,25 +1,48 @@
-# Import dependecies
+# import dependencies
 
 from bs4 import BeautifulSoup
 import csv
+import lxml
+import os
 
 
-def process_coordinate_string(str):    
-    # Take the coordinate string from the KML file, and break it up into [Lat,Lon,Lat,Lon...] for a CSV row
-    ret = []
-    comma_split = str.split(',')
-    return [comma_split[1], comma_split[0], comma_split[2]] #Return (lat, long, elevation)
+def process_coordinate_string(string):
+    # take the coordinate string from the KML file, and break it up into [Lat,Lon,Lat,Lon...] for a CSV row
+    comma_split = string.split(',')
+    return [comma_split[1], comma_split[0], comma_split[2]]     # return (lat, long, elevation)
 
-def main():    
+
+def kmlParser(Path):
     # Open the KML. Read the KML. Open a CSV file. Process a coordinate string to be a CSV row.
     # Make sure "profile.kml" or whatever the file is called is in the same directory
-    with open('profile.kml', 'r') as f:
-        s = BeautifulSoup(f, 'xml')
-        with open('profile.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
+    with open(Path, 'r') as f:
+        s = BeautifulSoup(f, 'lxml')
+        with open('profile.csv', 'w', newline='') as csvFile:
+            writer = csv.writer(csvFile)
 
+            # add header
+            writer.writerow(['Latitude', 'Longitude', 'Elevation'])
             for coords in s.find_all('coordinates'):
                 writer.writerow(process_coordinate_string(coords.string))
 
+            scriptDirectory = os.getcwd()
+            print(f'Conversion Complete. Please check [{scriptDirectory}] for the converted file.')
+
+            print("Press Enter to continue ...")
+            input()
+
+
+def pathInput():
+    print('KML to CSV Conversion Tool'.center(40, '='))
+    print('\nTo start your conversion, please enter one of the following options:')
+    print('\nOption-1: Please enter the path for your .kml file.')
+    print('''\nOption-2: Ensure that the selected KML file is located in the same directory as this script and
+please enter the filename for your .kml file.\n''')
+    Path = input('Input: ')
+    return Path
+
+
 # Run the main method
-main() 
+if __name__ == "__main__":
+    path = pathInput()
+    kmlParser(path)
