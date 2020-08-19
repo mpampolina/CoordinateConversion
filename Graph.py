2db336d82c11ae6d13ec174ea861190e3e29ed5e
BrendanLai <laibrendan8@gmail.com>
2020-08-18 22:31:50 -0400

    modified: Fixing graph scaling
    Fixing graph scaling by using cumulative distnace in metres intead of km. Also changed the get_file to get_path

--- Graph.py
+++ Graph.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-from BatchConversions import get_file
+from BatchConversions import get_path
 
 
-df = pd.read_csv(get_file())
+df = pd.read_csv(get_path())
 fig, ax = plt.subplots()
 my_scatter_plot = ax.scatter(
-    df["CumDistance (km)"],
+    df["CumDistance (m)"],
     df["Ho"]
 )
 
-ax.set_xlabel("Distance over Polyline (km)")
+ax.set_xlabel("Distance over Polyline (m)")
 ax.set_ylabel("Orthometric Elevation (m)")
 ax.set_title("Elevation on Polyline")
 
