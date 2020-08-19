4fc998cac9905758767a30440a449a66432c3915
BrendanLai <laibrendan8@gmail.com>
2020-08-18 21:56:33 -0400

    modified: file input and output formatting
    Will create the new ouptut file in the right directory and variable naming convention fixed. Also changed headings of the coordinates taken.

--- dxf_to_csv.py
+++ dxf_to_csv.py
@@ -1,16 +1,18 @@
 import csv
 import re
 import os
-from BatchConversions import get_file
+from BatchConversions import get_path, get_outputfilename
+from SingularConversionElevation import zoneValidation
 
 utmRegex = re.compile(r'''(AcDbPoint\n\s10\n(\d+\.\d+)\n\s20\n(\d+\.\d+)\n\s30\n(\d+\.\d+))
 ''', re.VERBOSE)
 
 
-def dxfParser(zone, Filename):
-    outputFilename = Filename.split('.')[0] + '_dxf_converted.csv' # File name is original file + string
+def dxfParser(zone, path):
     
-    with open(Filename, 'r') as myfile:
+    outputFilename = get_outputfilename()
+
+    with open(path, 'r') as myfile:
         data = myfile.read()    # reads in dxf.txt file and returns the data in string form
 
         data = utmRegex.findall(data)   # returns a list of tuples. Each tuple corresponds to a coordinate set
@@ -18,11 +20,11 @@ def dxfParser(zone, Filename):
         coordinateList = []
 
         for entry in data:
-            coordinates = {'Easting': entry[1], 'Northing': entry[2], 'Zone': zone, 'Elevation': entry[3]} # Structure of each csv line
+            coordinates = {'utm_e': entry[1], 'utm_n': entry[2], 'utm_z': zone, 'Elevation': entry[3]} # Structure of each csv line
             coordinateList.append(coordinates)
 
         with open(outputFilename, 'w', newline='') as csvFile:
-            fieldnames = ['Easting', 'Northing', 'Zone', 'Elevation']
+            fieldnames = ['utm_e', 'utm_n', 'utm_z', 'Elevation']
             writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
 
             writer.writeheader()
@@ -40,6 +42,6 @@ def get_zone():
     return int(input('\nInput: '))
 
 if __name__ == "__main__":
-    filename = get_file()
-    zone = get_zone()
-    dxfParser(zone, filename)
+    path = get_path()
+    zone = zoneValidation()
+    dxfParser(zone, path)
